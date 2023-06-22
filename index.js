5b410291a313f51c1f31cdf7ae41c520e05f42e6
desilvakdn <desilvakdn2@gmail.com>
2023-06-22 12:17:21 +0530

    fffeefffef

--- index.js
+++ index.js
@@ -27,17 +27,32 @@ const { google } = require("googleapis");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 const cors = require("cors");
-app.use(
+/* app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST"],
   })
-);
+); */
 
-app.options('*', cors({
-  origin: "*",
-  methods: ["GET", "POST"],
-}))
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.fiverr.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 
 const limiter = rateLimit({
   windowMs: 100000, // 1 minute
